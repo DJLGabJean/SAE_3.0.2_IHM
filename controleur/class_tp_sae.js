733c0a8cef75fcbe8b96fabf91e2cc4b3c89beb4
Raider <gabrielnae1@gmail.com>
2023-01-21 19:16:00 +0100

    Run Build Task

--- controleur/class_tp_sae.js
+++ controleur/class_tp_sae.js
@@ -115,6 +115,10 @@ class VueTpSae {
         this.form.divSelectionThemes.hidden = true;
         this.form.btnAbonnementRetour.hidden = true;
         this.form.divAbonnementTitre.innerHTML = "Ajout d'un abonnement";
+        //
+        const lesAbonnements = new LesAbonnements();
+        let numéroAbonnement = lesAbonnements.getNouveauNumero();
+        this.form.edtIdentificationAdh.value = numéroAbonnement;
     }
     afficherModifier() {
         if (this._grille.getIdSelect() !== "") {
